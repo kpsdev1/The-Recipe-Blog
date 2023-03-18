b5106c5b02accd26db4949dee2ed67f6e40a0a33
kpsdev1 <stuart151@hotmail.com>
2023-03-18 14:37:42 +0000

    changing back to safe tags for recipe details

--- recipeblog/settings.py
+++ recipeblog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['the-recipe-blog1.herokuapp.com', 'localhost']
 
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'wines',
 ]
 
-SUMMERNOTE_THEME = 'bs4'
+SUMMERNOTE_THEME = 'bs5'
 
 SUMMERNOTE_CONFIG = {
     'width': '100%',
